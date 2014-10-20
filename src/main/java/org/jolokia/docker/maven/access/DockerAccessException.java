339474389b18afaa0020c346bf0445571973eef1
Roland Huß <roland@jolokia.org>
2014-10-20 14:17:36 +0200

    Add missing exception

---
+++ src/main/java/org/jolokia/docker/maven/access/DockerAccessException.java
@@ -0,0 +1,23 @@
+package org.jolokia.docker.maven.access;
+
+/**
+ * Exception thrown if access to the docker host fails
+ *
+ * @author roland
+ * @since 20.10.14
+ */
+public class DockerAccessException extends Exception {
+
+    /**
+     *
+     * @param message error message
+     * @param cause root cause
+     */
+    public DockerAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DockerAccessException(String message) {
+        super(message);
+    }
+}
