b44453e1b43e677760b59a4b693826b15507d11b
Emily Bache <emily.bache@pagero.com>
2014-10-22 15:38:44 +0200

    The stop action appeared to do nothing whatsoever. So I added shutdown actions for configured containers.

--- src/main/java/org/jolokia/docker/maven/StopMojo.java
+++ src/main/java/org/jolokia/docker/maven/StopMojo.java
@@ -1,11 +1,12 @@
 package org.jolokia.docker.maven;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.*;
 import org.jolokia.docker.maven.access.DockerAccess;
+import org.jolokia.docker.maven.access.DockerAccessException;
+import org.jolokia.docker.maven.config.ImageConfiguration;
 
 /**
  * @author roland
@@ -26,8 +27,19 @@ public class StopMojo extends AbstractDockerMojo {
     @Parameter(property = "docker.keepData", defaultValue = "false")
     private boolean keepData;
 
-    protected void executeInternal(DockerAccess access) throws MojoExecutionException {
+    protected void executeInternal(DockerAccess access) throws MojoExecutionException, DockerAccessException {
         if (!keepRunning) {
+            getLog().info("about to stop " + this.images.size() + " docker image(s).");
+
+            Set<ShutdownAction> actions = Collections.synchronizedSet(new LinkedHashSet<ShutdownAction>());
+            for (ImageConfiguration image: images) {
+                for (String container: access.getContainersForImage(image.getName())) {
+                    getLog().debug("adding shutdown action for image " + image.getName());
+                    actions.add(new ShutdownAction(image.getName(), container));
+                }
+            }
+            getPluginContext().put(DOCKER_SHUTDOWN_ACTIONS, actions);
+
             List<ShutdownAction> appliedShutdownActions = new ArrayList<>();
             for (ShutdownAction action : getShutdownActionsInExecutionOrder()) {
                 action.shutdown(access, this, keepContainer);
