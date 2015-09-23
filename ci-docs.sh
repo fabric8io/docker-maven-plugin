57e23969f192be0588fee8699fc38e33bcaa9107
Roland Huß <roland@jolokia.org>
2015-09-23 10:24:21 +0200

    generated documentation

--- ci-docs.sh
+++ ci-docs.sh
@@ -2,13 +2,17 @@ echo ===========================================
 echo Deploying docker-maven-plugin documentation
 echo ===========================================
 
-cd docs && \
+cd doc/manual && \
 npm install -g gitbook-cli && \
-git clone -b gh-pages git@github.com:rhuss/docker-maven-plugin.git && \
+git clone -b gh-pages git@github.com:rhuss/docker-maven-plugin.git gh-pages && \
 mkdir -p _book && \
 gitbook install .  && \
 gitbook build . && \
 cp -rv _book/* gh-pages/ && \
+cd gh-pages && \
 git add * && \
 git commit -m "generated documentation" && \
-git push origin gh-pages
+git push origin gh-pages && \
+cd .. && \
+rm -rf gh-pages
+       
