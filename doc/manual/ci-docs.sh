26aba1fc2c13fd18084109b86a2a45d36c001c2f
Roland Huß <roland@jolokia.org>
2015-09-23 11:16:18 +0200

    changed order

--- doc/manual/ci-docs.sh
+++ doc/manual/ci-docs.sh
@@ -7,9 +7,9 @@ npm install -g gitbook-cli && \
 mkdir -p _book && \
 gitbook install .  && \
 gitbook build . && \
+git clone -b gh-pages git@github.com:rhuss/docker-maven-plugin.git gh-pages && \
 cp -rv _book/* gh-pages/ && \
 cd gh-pages && \
-git clone -b gh-pages git@github.com:rhuss/docker-maven-plugin.git gh-pages && \
 git add --ignore-errors * && \
 git commit -m "generated documentation" && \
 git push origin gh-pages && \
