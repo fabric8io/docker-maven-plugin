62d7ee3aee5a5df90b6e3c631a484d0dce3a29a7
Jae Gangemi <jgangemi@gmail.com>
2016-07-06 14:53:16 -0400

    - invoke the 'initalization' phase before all goals (except the 'source' gaoal which already does this by way of 'package')
    Signed-off-by: Jae Gangemi <jgangemi@gmail.com>

--- src/main/java/io/fabric8/maven/docker/WatchMojo.java
+++ src/main/java/io/fabric8/maven/docker/WatchMojo.java
@@ -35,6 +35,8 @@ import io.fabric8.maven.docker.util.MojoParameters;
 import io.fabric8.maven.docker.util.StartOrderResolver;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.StringUtils;
@@ -54,6 +56,7 @@ import org.codehaus.plexus.util.StringUtils;
  * @since 16/06/15
  */
 @Mojo(name = "watch")
+@Execute(phase = LifecyclePhase.INITIALIZE)
 public class WatchMojo extends AbstractBuildSupportMojo {
 
     /**
