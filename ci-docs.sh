debddb846bcdd6f4dbb98791f26790cf3c754cab
Roland Huß <roland@ro14nd.de>
2016-07-08 14:44:00 +0200

    generated documentation

---
+++ ci-docs.sh
@@ -0,0 +1,14 @@
+echo ===========================================
+echo Deploying docker-maven-plugin documentation
+echo ===========================================
+
+mvn -Pdoc-html &&
+git clone -b gh-pages git@github.com:fabric8io/docker-maven-plugin.git gh-pages && \
+cp -rv target/generated-docs/* gh-pages/ && \
+rm gh-pages/*~ && \
+cd gh-pages && \
+git add --ignore-errors * && \
+git commit -m "generated documentation" && \
+git push origin gh-pages && \
+cd .. && \
+rm -rf gh-pages target
