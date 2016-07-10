bcd06c651e30f6d49a6f4f89f778d68fcf94a582
CircleCI <circleci@mail.com>
2016-07-10 15:38:21 +0000

    generated documentation

--- ci-docs.sh
+++ ci-docs.sh
@@ -3,9 +3,11 @@ echo Deploying docker-maven-plugin documentation
 echo ===========================================
 
 mvn -Pdoc-html &&
+mvn -Pdoc-pdf &&
 git clone -b gh-pages git@github.com:fabric8io/docker-maven-plugin.git gh-pages && \
 cp -rv target/generated-docs/* gh-pages/ && \
 cd gh-pages && \
+mv index.pdf docker-maven-plugin.pdf && \
 git add --ignore-errors * && \
 git commit -m "generated documentation" && \
 git push origin gh-pages && \
