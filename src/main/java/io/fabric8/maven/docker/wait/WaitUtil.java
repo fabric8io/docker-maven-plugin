5ff32b03ddf6c8ad91979d2b2d8c09238d7940bb
janssk1 <koen.janssens@alcatel-lucent.com>
2017-04-10 11:51:33 +0200

    Make sure 'wait' ends if container no longer running

--- src/main/java/io/fabric8/maven/docker/wait/WaitUtil.java
+++ src/main/java/io/fabric8/maven/docker/wait/WaitUtil.java
@@ -1,5 +1,11 @@
 package io.fabric8.maven.docker.wait;
 
+import io.fabric8.maven.docker.access.DockerAccess;
+import io.fabric8.maven.docker.access.DockerAccessException;
+import io.fabric8.maven.docker.log.LogDispatcher;
+import io.fabric8.maven.docker.log.LogOutputSpec;
+
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.concurrent.*;
 
@@ -36,15 +42,28 @@ public class WaitUtil {
         return delta(now);
     }
 
-    public static long wait(int maxWait, WaitChecker ... checkers) throws WaitTimeoutException {
-        return wait(maxWait, Arrays.asList(checkers));
+    public static long wait(DockerAccess access, String containerId, int maxWait, WaitChecker ... checkers) throws WaitTimeoutException, NotRunningException {
+        return wait(access, containerId, maxWait, Arrays.asList(checkers));
     }
 
-    public static long wait(int maxWait, Iterable<WaitChecker> checkers) throws WaitTimeoutException {
+    public static long wait(DockerAccess access, String containerId, int maxWait, Iterable<WaitChecker> checkers) throws WaitTimeoutException, NotRunningException {
         long max = maxWait > 0 ? maxWait : DEFAULT_MAX_WAIT;
         long now = System.currentTimeMillis();
         try {
             do {
+                try {
+                    if (!access.getContainer(containerId).isRunning()) {
+                        //if not running, probably something went wrong during startup: spit out logs
+                        try {
+                            new LogDispatcher(access).fetchContainerLog(containerId, LogOutputSpec.DEFAULT);
+                        } catch (FileNotFoundException e) {
+                            //no logging
+                        }
+                        throw new NotRunningException("Container not running", delta(now));
+                    }
+                } catch (DockerAccessException e) {
+                    throw new NotRunningException("Unable to check container state: " + e.getMessage(), delta(now));
+                }
                 if (check(checkers)) {
                     return delta(now);
                 }
