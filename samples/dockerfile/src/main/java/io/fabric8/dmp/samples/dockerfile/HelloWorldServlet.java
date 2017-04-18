a8d2729b30aa8f7abe0756baf02e537cf4ecfcbf
Roland Huß <roland@ro14nd.de>
2017-04-18 09:17:41 +0200

    Update to read a file from the FS

--- samples/dockerfile/src/main/java/io/fabric8/dmp/samples/dockerfile/HelloWorldServlet.java
+++ samples/dockerfile/src/main/java/io/fabric8/dmp/samples/dockerfile/HelloWorldServlet.java
@@ -1,11 +1,15 @@
 package io.fabric8.dmp.samples.dockerfile;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 
 /**
  * @author roland
@@ -15,7 +19,8 @@ public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().append("Hello World !").flush();
+        String txt = FileUtils.readFileToString(new File("/welcome.txt"), Charset.defaultCharset());
+        resp.getWriter().append(txt).flush();
         resp.setHeader("Content-Type", "plain/text");
     }
 }
