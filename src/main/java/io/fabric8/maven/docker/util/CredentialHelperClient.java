31603a6ca0094cccb1d8972c38f85298dc38195a
Roland Huß <roland@ro14nd.de>
2018-01-16 11:10:07 +0100

    Changed to log level `verbose` when no response comes from the credential helper

--- src/main/java/io/fabric8/maven/docker/util/CredentialHelperClient.java
+++ src/main/java/io/fabric8/maven/docker/util/CredentialHelperClient.java
@@ -73,7 +73,7 @@ public class CredentialHelperClient {
         public String getVersion() throws IOException {
             execute();
             if (version == null) {
-                log.info("The credentials helper \"%s\" didn't return a version string",CredentialHelperClient.this.credentialHelperName);
+                log.verbose("The credentials helper \"%s\" didn't return a version string",CredentialHelperClient.this.credentialHelperName);
             }
             return version;
         }
