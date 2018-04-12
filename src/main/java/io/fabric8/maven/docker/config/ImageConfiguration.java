8eea9200696ca142245debc15635fd7624187f78
Roland Huß <roland@ro14nd.de>
2018-04-12 11:57:35 +0200

    chore: Optimized imports

--- src/main/java/io/fabric8/maven/docker/config/ImageConfiguration.java
+++ src/main/java/io/fabric8/maven/docker/config/ImageConfiguration.java
@@ -1,11 +1,16 @@
 package io.fabric8.maven.docker.config;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import io.fabric8.maven.docker.util.*;
+import io.fabric8.maven.docker.util.DeepCopy;
+import io.fabric8.maven.docker.util.EnvUtil;
+import io.fabric8.maven.docker.util.ImageName;
+import io.fabric8.maven.docker.util.Logger;
+import io.fabric8.maven.docker.util.StartOrderResolver;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 
 /**
  * @author roland
