d74b2e36b61e4578cd1c959b20672551b374eb03
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2020-01-17 09:01:58 +0100

    Cleanup

--- src/test/java/io/fabric8/maven/docker/util/aws/AwsSdkAuthConfigFactoryTest.java
+++ src/test/java/io/fabric8/maven/docker/util/aws/AwsSdkAuthConfigFactoryTest.java
@@ -2,14 +2,11 @@ package io.fabric8.maven.docker.util.aws;
 
 import io.fabric8.maven.docker.access.AuthConfig;
 import io.fabric8.maven.docker.util.Logger;
-import mockit.Expectations;
 import mockit.Mocked;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
-
-import java.util.UUID;
 
 import static java.util.UUID.randomUUID;
 import static org.junit.Assert.assertEquals;
@@ -72,70 +69,4 @@ public class AwsSdkAuthConfigFactoryTest {
         assertNull(authConfig.getIdentityToken());
     }
 
-    /*
-    @Test
-    public void exceptionsAreHandledGracefully() {
-        new Expectations() {{
-            credentialsProvider.getCredentials();
-            minTimes = 1;
-            result = new SdkClientException("Unauthorized");
-        }};
-
-        AuthConfig authConfig = objectUnderTest.createAuthConfig();
-
-        assertNull(authConfig);
-    }
-
-    @Test
-    public void nullValueIsPassedOn() {
-        new Expectations() {{
-            credentialsProvider.getCredentials();
-            minTimes = 1;
-            result = null;
-        }};
-
-        AuthConfig authConfig = objectUnderTest.createAuthConfig();
-
-        assertNull(authConfig);
-    }
-
-    @Test
-    public void basicCredentialsAreTransformedIntoAuthConfig() {
-        String accessKey = "accessKey";
-        String secretKey = "secretKey";
-        new Expectations() {{
-            credentialsProvider.getCredentials();
-            minTimes = 1;
-            this.result = new BasicAWSCredentials(accessKey, secretKey);
-        }};
-
-        AuthConfig authConfig = objectUnderTest.createAuthConfig();
-
-        assertNotNull(authConfig);
-        assertEquals(accessKey, authConfig.getUsername());
-        assertEquals(secretKey, authConfig.getPassword());
-        assertNull(authConfig.getAuth());
-        assertNull(authConfig.getIdentityToken());
-    }
-
-    @Test
-    public void sessionCredentialsAreTransformedIntoAuthConfig() {
-        String accessKey = "accessKey";
-        String secretKey = "secretKey";
-        String sessionToken = "sessionToken";
-        new Expectations() {{
-            credentialsProvider.getCredentials();
-            minTimes = 1;
-            this.result = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
-        }};
-
-        AuthConfig authConfig = objectUnderTest.createAuthConfig();
-
-        assertNotNull(authConfig);
-        assertEquals(accessKey, authConfig.getUsername());
-        assertEquals(secretKey, authConfig.getPassword());
-        assertEquals(sessionToken, authConfig.getAuth());
-        assertNull(authConfig.getIdentityToken());
-    }
-*/
 }
