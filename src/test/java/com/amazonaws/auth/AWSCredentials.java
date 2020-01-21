b6d425dea625762310560bae00b91817d1e6d357
Roland Huß <roland@ro14nd.de>
2020-01-21 09:40:26 +0100

    Some minor cosmetics (#1321)

--- src/test/java/com/amazonaws/auth/AWSCredentials.java
+++ src/test/java/com/amazonaws/auth/AWSCredentials.java
@@ -1,8 +1,24 @@
 package com.amazonaws.auth;
 
+/*
+ * Copyright 2011-2020 Amazon Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *    http://aws.amazon.com/apache2.0
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
- * Shameless copy of the original for testing {@link io.fabric8.maven.docker.util.aws.AwsSdkAuthConfigFactory}.
- * Based on <tt>com.amazonaws:aws-java-sdk-core:1.11.707</tt>.
+ * Copy of the original class for testing {@link io.fabric8.maven.docker.util.aws.AwsSdkAuthConfigFactory}.
+ * Based on <a href="https://github.com/aws/aws-sdk-java/blob/1.11.707/aws-java-sdk-core/src/main/java/com/amazonaws/auth/AWSCredentials.java">com.amazonaws:aws-java-sdk-core:1.11.707</a> (also APL licensed).
+ * We can't use a direct dependency here, as we have
+ * to keep d-m-p agnostic of the AWS SDK and only access it via reflection.
  */
 public class AWSCredentials {
     private final String accessKeyId;
