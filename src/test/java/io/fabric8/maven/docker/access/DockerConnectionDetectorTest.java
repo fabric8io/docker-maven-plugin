1732502e8123be864694958a5d4f3de81774ab1c
Ondřej Mach <ondrej.mach@certicon.cz>
2020-12-02 18:22:48 +0100

    Fix Windows Tests
    + fix path handling to be non-linux specific
    + cleanup path transformations

--- src/test/java/io/fabric8/maven/docker/access/DockerConnectionDetectorTest.java
+++ src/test/java/io/fabric8/maven/docker/access/DockerConnectionDetectorTest.java
@@ -24,7 +24,7 @@ public class DockerConnectionDetectorTest {
         String dockerHost = System.getenv("DOCKER_HOST");
         if (dockerHost != null) {
             assertEquals(dockerHost.replaceFirst("^tcp:/",""), detector.detectConnectionParameter(null, null).getUrl().replaceFirst("^https?:/", ""));
-        } else if (System.getProperty("os.name").equalsIgnoreCase("Windows 10")) {
+        } else if (System.getProperty("os.name").contains("Windows")) {
         	try {
                 assertEquals("npipe:////./pipe/docker_engine", detector.detectConnectionParameter(null, null).getUrl());
             } catch (IllegalArgumentException expectedIfNoUnixSocket) {
