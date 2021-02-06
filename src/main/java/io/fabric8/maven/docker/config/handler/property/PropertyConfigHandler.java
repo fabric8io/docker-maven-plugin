a324a4d6aba28e2f40f3808863fabf23c3e6da0e
Caitlin Elfring <celfring@gmail.com>
2021-02-06 12:21:26 -0500

    Include `assembly.inline` when using external properties
    Add test to show `assembly.inline` is removed when external properties are enabled
    
    Signed-off-by: Caitlin Elfring <celfring@gmail.com>

--- src/main/java/io/fabric8/maven/docker/config/handler/property/PropertyConfigHandler.java
+++ src/main/java/io/fabric8/maven/docker/config/handler/property/PropertyConfigHandler.java
@@ -247,6 +247,7 @@ public class PropertyConfigHandler implements ExternalConfigHandler {
                 .user(valueProvider.getString(ASSEMBLY_USER, config == null ? null : config.getUser()))
                 .mode(valueProvider.getString(ASSEMBLY_MODE, config == null ? null : config.getModeRaw()))
                 .tarLongFileMode(valueProvider.getString(ASSEMBLY_TARLONGFILEMODE, config == null ? null : config.getTarLongFileMode()))
+                .assemblyDef(config == null ? null : config.getInline())
                 .build();
     }
 
