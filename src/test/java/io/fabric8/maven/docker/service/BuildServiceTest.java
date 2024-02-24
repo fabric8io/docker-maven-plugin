fabric8io/docker-maven-plugin

commit 452123d1385f670950c5edb86acf9af088e99b78

Add support for using maven/system properties for an ARG used as the FROM image in a dockerfile

it/pom.xml
@@ -30,6 +30,7 @@
     <module>docker-compose</module>
     <module>docker-compose-dependon</module>
     <module>dockerfile</module>
+    <module>dockerfile-base-as-arg</module>
     <module>dockerignore</module>
     <module>healthcheck</module>
     <module>helloworld</module>

src/main/java/io/fabric8/maven/docker/service/BuildService.java
@@ -64,12 +64,13 @@ public class BuildService {
     public void buildImage(ImageConfiguration imageConfig, ImagePullManager imagePullManager, BuildContext buildContext, File buildArchiveFile)
             throws DockerAccessException, MojoExecutionException {
 
+        Map<String, String> buildArgs = addBuildArgs(buildContext);
         if (imagePullManager != null) {
-            autoPullBaseImage(imageConfig, imagePullManager, buildContext);
+            autoPullBaseImage(imageConfig, imagePullManager, buildContext, buildArgs);
             autoPullCacheFromImage(imageConfig, imagePullManager, buildContext);
         }
 
-        buildImage(imageConfig, buildContext.getMojoParameters(), ConfigHelper.isNoCache(imageConfig), checkForSquash(imageConfig), addBuildArgs(buildContext), buildArchiveFile);
+        buildImage(imageConfig, buildContext.getMojoParameters(), ConfigHelper.isNoCache(imageConfig), checkForSquash(imageConfig), buildArgs, buildArchiveFile);
     }
 
     /**
@@ -357,7 +358,7 @@ public class BuildService {
         return buildArgs;
     }
 
-    private void autoPullBaseImage(ImageConfiguration imageConfig, ImagePullManager imagePullManager, BuildContext buildContext)
+    private void autoPullBaseImage(ImageConfiguration imageConfig, ImagePullManager imagePullManager, BuildContext buildContext, Map<String, String> buildArgs)
             throws DockerAccessException, MojoExecutionException {
         BuildImageConfiguration buildConfig = imageConfig.getBuildConfiguration();
         CleanupMode cleanupMode = buildConfig.cleanupMode();
@@ -369,12 +370,12 @@ public class BuildService {
 
         List<String> fromImages;
         if (buildConfig.isDockerFileMode()) {
-            fromImages = extractBaseFromDockerfile(buildConfig, buildContext);
+            fromImages = extractBaseFromDockerfile(buildConfig, buildContext, buildArgs);
         } else {
             fromImages = new LinkedList<>();
             String baseImage = extractBaseFromConfiguration(buildConfig);
             if (baseImage != null) {
-                fromImages.add(extractBaseFromConfiguration(buildConfig));
+                fromImages.add(baseImage);
             }
         }
         for (String fromImage : fromImages) {
@@ -431,14 +432,14 @@ public class BuildService {
         return fromImage;
     }
 
-    private List<String> extractBaseFromDockerfile(BuildImageConfiguration buildConfig, BuildContext buildContext) {
+    private List<String> extractBaseFromDockerfile(BuildImageConfiguration buildConfig, BuildContext buildContext, Map<String, String> buildArgs) {
         List<String> fromImage;
         try {
             File fullDockerFilePath = buildConfig.getAbsoluteDockerFilePath(buildContext.getMojoParameters());
             fromImage = DockerFileUtil.extractBaseImages(
                     fullDockerFilePath,
                     DockerFileUtil.createInterpolator(buildContext.getMojoParameters(), buildConfig.getFilter()),
-                    buildConfig.getArgs());
+                    buildArgs);
         } catch (IOException e) {
             // Cant extract base image, so we wont try an auto pull. An error will occur later anyway when
             // building the image, so we are passive here.

it/dockerfile-base-as-arg/pom.xml
@@ -1,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+
+  <!--
+  Simple sample program including Java code.
+
+  This helloworld exactly has been taken over mostly from https://github.com/arun-gupta/docker-java-sample.git
+-->
+
+  <modelVersion>4.0.0</modelVersion>
+
+  <parent>
+    <groupId>io.fabric8.dmp.itests</groupId>
+    <artifactId>dmp-it-parent</artifactId>
+    <version>0.44-SNAPSHOT</version>
+    <relativePath>../pom.xml</relativePath>
+  </parent>
+
+  <artifactId>dmp-it-dockerfile-base-as-arg</artifactId>
+  <version>0.44-SNAPSHOT</version>
+  <packaging>war</packaging>
+  <name>dmp-it-dockerfile</name>
+
+  <properties>
+    <docker.buildArg.baseImage>jetty</docker.buildArg.baseImage>
+    <file>welcome.txt</file>
+    <project.artifactId>${project.artifactId}</project.artifactId>
+  </properties>
+
+  <dependencies>
+    <dependency>
+      <groupId>commons-io</groupId>
+      <artifactId>commons-io</artifactId>
+      <version>2.7</version>
+    </dependency>
+    <dependency>
+      <groupId>javax.servlet</groupId>
+      <artifactId>javax.servlet-api</artifactId>
+      <version>3.1.0</version>
+    </dependency>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>4.13.1</version>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>io.fabric8</groupId>
+        <artifactId>docker-maven-plugin</artifactId>
+        <configuration>
+          <images>
+            <image>
+              <name>fabric8:dmp-it-dockerfile</name>
+              <alias>dockerfile</alias>
+              <build>
+                <!-- filter>@</filter-->
+                <contextDir>${project.basedir}/src/main/docker</contextDir>
+                <assembly>
+                  <descriptorRef>rootWar</descriptorRef>
+                </assembly>
+              </build>
+              <run>
+                <ports>
+                  <port>8080:8080</port>
+                </ports>
+              </run>
+            </image>
+          </images>
+        </configuration>
+        <executions>
+          <execution>
+            <id>build</id>
+            <goals>
+              <goal>build</goal>
+            </goals>
+            <phase>install</phase>
+          </execution>
+          <execution>
+            <id>start</id>
+            <goals>
+              <goal>start</goal>
+            </goals>
+            <phase>install</phase>
+          </execution>
+          <execution>
+            <id>stop</id>
+            <goals>
+              <goal>stop</goal>
+            </goals>
+            <phase>install</phase>
+          </execution>
+        </executions>
+      </plugin>
+    </plugins>
+  </build>
+</project>

it/dockerfile-base-as-arg/src/main/docker/Dockerfile
@@ -1,0 +1,16 @@
+# Sample Dockerfile for use with the Docker file mode
+ARG baseImage
+FROM ${baseImage}
+
+ENV SAMPLE_BUILD_MODE=dockerfile
+LABEL PROJECT_NAME=hello-world \
+      PROJECT=${project.artifactId}
+
+# Arbitrary files can be added
+ADD ${file} /
+
+# In maven/ the files as specified in the <assembly> section is stored
+# and need to be added manually
+COPY maven/ /var/lib/jetty/webapps/
+
+EXPOSE 8080

it/dockerfile-base-as-arg/src/main/java/io/fabric8/dmp/samples/dockerfile/HelloWorldServlet.java
@@ -1,0 +1,26 @@
+package io.fabric8.dmp.itests.dockerfile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @author roland
+ * @since 18.04.17
+ */
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String txt = FileUtils.readFileToString(new File("/welcome.txt"), Charset.defaultCharset());
+        resp.getWriter().append(txt).flush();
+        resp.setHeader("Content-Type", "plain/text");
+    }
+}

it/dockerfile-base-as-arg/src/main/webapp/WEB-INF/web.xml
@@ -1,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<web-app version="3.0" xmlns="http://java.sun.com/xml/ns/javaee"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd">
+
+  <servlet>
+    <display-name>HelloWold</display-name>
+    <servlet-name>hello-world</servlet-name>
+    <servlet-class>io.fabric8.dmp.itests.dockerfile.HelloWorldServlet</servlet-class>
+  </servlet>
+
+  <servlet-mapping>
+    <servlet-name>hello-world</servlet-name>
+    <url-pattern>/</url-pattern>
+  </servlet-mapping>
+
+</web-app>

commit 8fa0c7b3146c3824205bc56871360f927f6fa091

fix (BuildService) : Image Build Config BuildArgs should be passed while pulling images (#1756)
Fixes regression introduced by #1731

In #1731, we added support for specifying docker build args from
maven/system properties. However, I missed it in review that build args
specified in plugin image build configuration are no longer passed to
autoPullBaseImage.

Ensure we merge image build configuration build args with args provided
via maven/system properties before passing them to autoPullBaseImage

Signed-off-by: Rohan Kumar <rohaan@redhat.com>

it/dockerfile-base-as-arg-buildconfig/pom.xml
@@ -1,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+
+  <parent>
+    <groupId>io.fabric8.dmp.itests</groupId>
+    <artifactId>dmp-it-parent</artifactId>
+    <version>0.45-SNAPSHOT</version>
+    <relativePath>../pom.xml</relativePath>
+  </parent>
+
+  <artifactId>dmp-it-dockerfile-base-as-arg-buildconfig</artifactId>
+  <version>0.45-SNAPSHOT</version>
+  <name>dmp-it-dockerfile-base-as-arg-buildconfig</name>
+
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>io.fabric8</groupId>
+        <artifactId>docker-maven-plugin</artifactId>
+        <configuration>
+          <images>
+            <image>
+              <name>fabric8:dmp-it-dockerfile-base-as-arg-buildconfig</name>
+              <build>
+                <dockerFile>Dockerfile</dockerFile>
+                <args>
+                  <FROM_IMAGE>openjdk:21-slim</FROM_IMAGE>
+                </args>
+              </build>
+            </image>
+          </images>
+        </configuration>
+        <executions>
+          <execution>
+            <id>build</id>
+            <goals>
+              <goal>build</goal>
+            </goals>
+            <phase>install</phase>
+          </execution>
+        </executions>
+      </plugin>
+    </plugins>
+  </build>
+</project>

it/dockerfile-base-as-arg-buildconfig/src/main/docker/Dockerfile
@@ -1,0 +1,2 @@
+ARG FROM_IMAGE
+FROM ${FROM_IMAGE} AS jlink

it/pom.xml
@@ -31,6 +31,7 @@
     <module>docker-compose-dependon</module>
     <module>dockerfile</module>
     <module>dockerfile-base-as-arg</module>
+    <module>dockerfile-base-as-arg-buildconfig</module>
     <module>dockerignore</module>
     <module>healthcheck</module>
     <module>helloworld</module>

src/main/java/io/fabric8/maven/docker/service/BuildService.java
@@ -66,7 +66,7 @@ public class BuildService {
 
         Map<String, String> buildArgs = addBuildArgs(buildContext);
         if (imagePullManager != null) {
-            autoPullBaseImage(imageConfig, imagePullManager, buildContext, buildArgs);
+            autoPullBaseImage(imageConfig, imagePullManager, buildContext, prepareBuildArgs(buildArgs, imageConfig.getBuildConfiguration()));
             autoPullCacheFromImage(imageConfig, imagePullManager, buildContext);
         }
 

src/test/java/io/fabric8/maven/docker/service/BuildServiceTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.junit.jupiter.api.Assertions;
@@ -61,6 +62,9 @@ class BuildServiceTest {
 
     @Mock
     MavenProject mavenProject;
+
+    @Mock
+    MavenSession mavenSession;
 
     @Mock
     private QueryService queryService;
@@ -189,6 +193,35 @@ class BuildServiceTest {
         //verify that tries to pull both images
         verifyImagePull(buildConfig, pullManager, buildContext, "fabric8/s2i-java");
         verifyImagePull(buildConfig, pullManager, buildContext, "fabric8/s1i-java");
+    }
+
+    @Test
+    void testDockerfileWithBuildArgsInBuildConfig_ShouldPullImage() throws Exception {
+        BuildImageConfiguration buildConfig = new BuildImageConfiguration.Builder()
+            .dockerFile(getClass().getResource("/io/fabric8/maven/docker/util/Dockerfile_from_build_arg").getPath())
+            .args(Collections.singletonMap("FROM_IMAGE", "sample/base-image:latest"))
+            .build();
+
+        buildConfig.initAndValidate(logger);
+
+        imageConfig = new ImageConfiguration.Builder()
+            .name("build-image")
+            .buildConfig(buildConfig)
+            .build();
+
+        final ImagePullManager pullManager = new ImagePullManager(null, null, null);
+        final BuildService.BuildContext buildContext = new BuildService.BuildContext.Builder()
+            .mojoParameters(mojoParameters)
+            .build();
+
+        Mockito.when(mojoParameters.getSession()).thenReturn(mavenSession);
+        mockMavenProject();
+
+        File buildArchive = buildService.buildArchive(imageConfig, buildContext, "");
+        buildService.buildImage(imageConfig, pullManager, buildContext, buildArchive);
+
+        //verify that tries to pull both images
+        verifyImagePull(buildConfig, pullManager, buildContext, "sample/base-image:latest");
     }
 
     @Test

src/test/resources/io/fabric8/maven/docker/util/Dockerfile_from_build_arg
@@ -1,0 +1,2 @@
+ARG FROM_IMAGE
+FROM ${FROM_IMAGE} AS jlink