849b1d1b33339ff8f90ed3608e9a4c33a5b3e1bc
Zixuan Liu <nodeces@gmail.com>
2024-05-23 00:20:32 +0800

    Improve test

--- src/test/java/io/fabric8/maven/docker/TagMojoTest.java
+++ src/test/java/io/fabric8/maven/docker/TagMojoTest.java
@@ -9,8 +9,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -23,38 +26,47 @@ class TagMojoTest extends MojoTestBase {
     @InjectMocks
     private TagMojo tagMojo;
 
-    void tag(boolean skipTag, boolean buildx) throws IOException, MojoExecutionException {
-        givenMavenProject(tagMojo);
+    @Nested
+    class NormalBuild {
+        @ParameterizedTest
+        @ValueSource(booleans = {true, false})
+        @DisplayName("If skipTag is true, call tag; otherwise, do not call")
+        void tag(boolean skipTag) throws IOException, MojoExecutionException {
+            givenMavenProject(tagMojo);
 
-        ImageConfiguration imageConfiguration = singleImageConfiguration(builder -> {
-            builder.skipTag(skipTag);
-            if (buildx) {
+            ImageConfiguration imageConfiguration = singleImageConfiguration(builder -> {
+                builder.skipTag(skipTag);
+            });
+            givenResolvedImages(tagMojo, Collections.singletonList(imageConfiguration));
+
+            tagMojo.executeInternal(serviceHub);
+
+            verify(buildService, times(skipTag ? 0 : 1)).tagImage(any(), any(), any(), any());
+        }
+    }
+
+    @Nested
+    class BuildX {
+        @ParameterizedTest
+        @ValueSource(booleans = {true, false})
+        @DisplayName("Always skip tag")
+        void tag(boolean skipTag) throws IOException, MojoExecutionException {
+            givenMavenProject(tagMojo);
+
+            ImageConfiguration imageConfiguration = singleImageConfiguration(builder -> {
+                builder.skipTag(skipTag);
                 ArrayList<String> platforms = new ArrayList<>();
                 platforms.add(NON_NATIVE_PLATFORM);
                 platforms.add(NATIVE_PLATFORM);
-                BuildXConfiguration buildXConfiguration = new BuildXConfiguration.Builder().platforms(platforms).build();
+                BuildXConfiguration buildXConfiguration =
+                        new BuildXConfiguration.Builder().platforms(platforms).build();
                 builder.buildx(buildXConfiguration);
-            }
-        });
-        givenResolvedImages(tagMojo, Collections.singletonList(imageConfiguration));
+            });
+            givenResolvedImages(tagMojo, Collections.singletonList(imageConfiguration));
 
-        tagMojo.executeInternal(serviceHub);
+            tagMojo.executeInternal(serviceHub);
 
-        verify(buildService, times(skipTag || buildx ? 0 : 1)).tagImage(any(), any(), any(), any());
-    }
-
-    @Test
-    void skipTag() throws IOException, MojoExecutionException {
-        tag(true, false);
-    }
-
-    @Test
-    void tag() throws IOException, MojoExecutionException {
-        tag(false, false);
-    }
-
-    @Test
-    void tagWhenUsingBuildx() throws IOException, MojoExecutionException {
-        tag(false, true);
+            verify(buildService, times(0)).tagImage(any(), any(), any(), any());
+        }
     }
 }
