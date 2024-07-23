8a4853cea9dc2f4aa4691a88e779521b79729e19
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-07-23 23:37:38 +0530

    Improve it/spring-boot-with-jib integration test
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- it/spring-boot-with-jib/src/main/java/io/fabric8/maven/sample/springboot/jib/HelloController.java
+++ it/spring-boot-with-jib/src/main/java/io/fabric8/maven/sample/springboot/jib/HelloController.java
@@ -1,6 +1,6 @@
 package io.fabric8.maven.sample.springboot.jib;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
