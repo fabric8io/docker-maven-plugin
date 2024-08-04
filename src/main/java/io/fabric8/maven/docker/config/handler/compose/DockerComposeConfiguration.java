09cb972a4d7c82d4457e518e9ee1b1c618c87277
Sadiq Ahmed Killedar <125464939+Er-Sadiq@users.noreply.github.com>
2024-08-04 21:06:32 +0530

    "Refactor to use getOrDefault method for default values #59"

--- src/main/java/io/fabric8/maven/docker/config/handler/compose/DockerComposeConfiguration.java
+++ src/main/java/io/fabric8/maven/docker/config/handler/compose/DockerComposeConfiguration.java
@@ -7,11 +7,13 @@ public class DockerComposeConfiguration {
     private final String basedir;
     private final String composeFile;
     private final boolean ignoreBuild;
+    
     public DockerComposeConfiguration(Map<String, String> config) {
-        basedir = config.containsKey("basedir") ? config.get("basedir") : "src/main/docker";
-        composeFile = config.containsKey("composeFile") ? config.get("composeFile") : "docker-compose.yml";
-        ignoreBuild = config.containsKey("ignoreBuild") ? Boolean.parseBoolean(config.get("ignoreBuilder")) : false;
+    basedir = config.getOrDefault("basedir", "src/main/docker");
+    composeFile = config.getOrDefault("composeFile", "docker-compose.yml");
+    ignoreBuild = Boolean.parseBoolean(config.getOrDefault("ignoreBuild", "false"));
     }
+
 
     String getBasedir() {
         return basedir;
