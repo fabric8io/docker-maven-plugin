177c3b1826789c928c33e1a2727efc65312694f8
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-08-04 16:14:33 +0530

    improved it/spring-boot-with-jib/src/main/java/io/fabric8/maven/sample/springboot/jib/HelloController.java
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- it/spring-boot-with-jib/src/main/java/io/fabric8/maven/sample/springboot/jib/HelloController.java
+++ it/spring-boot-with-jib/src/main/java/io/fabric8/maven/sample/springboot/jib/HelloController.java
@@ -1,13 +1,13 @@
 package io.fabric8.maven.sample.springboot.jib;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "<h1>Greetings from Spring Boot(Powered by JIB)!!</h1>";
     }
