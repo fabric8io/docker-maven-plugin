b0afa8bdd64d58c2a745be72c3f727018f104879
Rohan Kumar <rohaan@redhat.com>
2025-02-08 21:16:58 +0530

    review : remove wildcard imports and use isEmpty from Apache commons
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- src/main/java/io/fabric8/maven/docker/util/ContainerNamingUtil.java
+++ src/main/java/io/fabric8/maven/docker/util/ContainerNamingUtil.java
@@ -1,6 +1,13 @@
 package io.fabric8.maven.docker.util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
