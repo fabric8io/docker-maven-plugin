673fb87834f53a4b3811224b1d3c7da30fe975b5
Sadiq Ahmed Killedar <125464939+Er-Sadiq@users.noreply.github.com>
2025-06-18 15:03:34 +0530

    "Refactor to use getOrDefault method for default values #59" (#1815)
    * "Refactor to use getOrDefault method for default values #59"
    
    * Refactor DockerComposeConfiguration to Use getOrDefault for Default Values and fixed the indentation
    
    * Refactor DockerComposeConfiguration to Use getOrDefault for Default Values and fixed the indentation issue:#1814
    
    * review : fix indentation in DockerComposeConfiguration
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>
    
    ---------
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>
    Co-authored-by: Rohan Kumar <rohaan@redhat.com>

--- src/main/java/io/fabric8/maven/docker/config/handler/compose/DockerComposeConfiguration.java
+++ src/main/java/io/fabric8/maven/docker/config/handler/compose/DockerComposeConfiguration.java
@@ -7,10 +7,11 @@ public class DockerComposeConfiguration {
     private final String basedir;
     private final String composeFile;
     private final boolean ignoreBuild;
-    public DockerComposeConfiguration(Map<String, String> config) {
-        basedir = config.containsKey("basedir") ? config.get("basedir") : "src/main/docker";
-        composeFile = config.containsKey("composeFile") ? config.get("composeFile") : "docker-compose.yml";
-        ignoreBuild = config.containsKey("ignoreBuild") ? Boolean.parseBoolean(config.get("ignoreBuilder")) : false;
+    
+    public DockerComposeConfiguration(Map<String, String> config) {    
+        basedir = config.getOrDefault("basedir", "src/main/docker");
+        composeFile = config.getOrDefault("composeFile", "docker-compose.yml");
+        ignoreBuild = Boolean.parseBoolean(config.getOrDefault("ignoreBuild", "false"));
     }
 
     String getBasedir() {
